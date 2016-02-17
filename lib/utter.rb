77a88ee56991997ced536cdc1c2e34c83bfd43b9
parasquid <tristan.gomez@gmail.com>
2016-02-17 18:44:40 +0800

    some renaming of functions and cleanup

--- lib/utter.rb
+++ lib/utter.rb
@@ -5,21 +5,20 @@ module Utter
   include Observable
 
   def utter(event, payload=nil)
-    events = utter_events[event.to_sym]
-    events.each do |block|
+    events[event.to_sym].each do |block|
       block.call
     end
   end
 
   def on(event, &block)
-    utter_events[event.to_sym].push block
+    events[event.to_sym].push block
+  end
+
+  def events
+    @events ||= events_table
   end
 
   private
-
-  def utter_events
-    @utter_events ||= events_table
-  end
 
   def events_table
     Hash.new { |hash, key| hash[key] = [] }
