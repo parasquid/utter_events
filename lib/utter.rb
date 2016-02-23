35ed3e2acb3e1ec60f543d93697d9af2da9363f0
parasquid <tristan.gomez@gmail.com>
2016-02-23 10:39:01 +0800

    refactor to use a global events table
    this is very interesting because the code is becoming shorter and shorter;
    however it's actually getting more and more complicated.
    
    the tests are enabling me to make very aggressive refactorings that will give
    me the flexibility i need for future design decisions.

--- lib/utter.rb
+++ lib/utter.rb
@@ -3,26 +3,24 @@ require "observer"
 
 module Utter
   module Sinks
-    EVENTS_TABLE = Hash.new { |hash, key| hash[key] = [] }
+    GLOBAL_EVENTS_TABLE = Hash.new { |hash, key|
+      hash[key] = Hash.new { |h, k|
+        h[k] = []
+      }
+    }
   end
 
   def utter(event, payload=nil)
-    events[event.to_sym].each do |block|
+    events[self][event.to_sym].each do |block|
       block.call(payload)
     end
   end
 
   def on(event, &block)
-    events[event.to_sym].push block
+    events[self][event.to_sym].push block
   end
 
   def events
-    @events ||= local_events_table
-  end
-
-  private
-
-  def local_events_table
-    Hash.new { |hash, key| hash[key] = [] }
+    Sinks::GLOBAL_EVENTS_TABLE
   end
 end
